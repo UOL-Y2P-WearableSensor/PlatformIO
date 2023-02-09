c2fe4733e7e191b6f9a713a9c7fcd7078b8c6ab4
Yixiao <1411101569@qq.com>
2023-02-09 23:06:10 +0000

    feat: able to connect 2.4G wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,114 @@
 /*
-  Arduino LSM6DS3 - Simple Gyroscope
+ This example connects to an unencrypted WiFi network.
+ Then it prints the MAC address of the WiFi module,
+ the IP address obtained, and other network details.
 
-  This example reads the gyroscope values from the LSM6DS3
-  sensor and continuously prints them to the Serial Monitor
-  or Serial Plotter.
+ created 13 July 2010
+ by dlf (Metodo2 srl)
+ modified 31 May 2012
+ by Tom Igoe
+ */
+#include <SPI.h>
+#include <WiFiNINA.h>
 
-  The circuit:
-  - Arduino Uno WiFi Rev 2 or Arduino Nano 33 IoT
+#define SECRET_SSID "CastleBlack"
+#define SECRET_PASS "123123123"
 
-  created 10 Jul 2019
-  by Riccardo Rizzo
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
 
-  This example code is in the public domain.
-*/
+void printMacAddress(byte mac[]) {
+    for (int i = 5; i >= 0; i--) {
+        if (mac[i] < 16) {
+            Serial.print("0");
+        }
+        Serial.print(mac[i], HEX);
+        if (i > 0) {
+            Serial.print(":");
+        }
+    }
+    Serial.println();
+}
 
-#include <Arduino_LSM6DS3.h>
-#include <UART.h>
+void printWifiData() {
+    // print your board's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+    Serial.println(ip);
+
+    // print your MAC address:
+    byte mac[6];
+    WiFi.macAddress(mac);
+    Serial.print("MAC address: ");
+    printMacAddress(mac);
+}
+
+void printCurrentNet() {
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print the MAC address of the router you're attached to:
+    byte bssid[6];
+    WiFi.BSSID(bssid);
+    Serial.print("BSSID: ");
+    printMacAddress(bssid);
+
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.println(rssi);
+
+    // print the encryption type:
+    byte encryption = WiFi.encryptionType();
+    Serial.print("Encryption Type:");
+    Serial.println(encryption, HEX);
+    Serial.println();
+}
+
 
 void setup() {
+    //Initialize serial and wait for port to open:
     Serial.begin(9600);
-    while (!Serial);
-
-    if (!IMU.begin()) {
-        Serial.println("Failed to initialize IMU!");
-
-        while (1);
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB port only
     }
 
-    Serial.print("Gyroscope sample rate = ");
-    Serial.print(IMU.gyroscopeSampleRate());
-    Serial.println(" Hz");
-    Serial.println();
-    Serial.println("Gyroscope in degrees/second");
-    Serial.println("X\tY\tZ");
+    // check for the WiFi module:
+    if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("Communication with WiFi module failed!");
+        // don't continue
+        while (true);
+    }
+
+    String fv = WiFi.firmwareVersion();
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+        Serial.println("Please upgrade the firmware");
+    }
+
+    // attempt to connect to WiFi network:
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to WPA SSID: ");
+        Serial.println(ssid);
+        // Connect to WPA/WPA2 network:
+        status = WiFi.begin(ssid, pass);
+
+        // wait 10 seconds for connection:
+        delay(10000);
+    }
+
+    // you're connected now, so print out the data:
+    Serial.print("You're connected to the network");
+    printCurrentNet();
+    printWifiData();
+
 }
 
 void loop() {
-    float x, y, z;
-
-    if (IMU.gyroscopeAvailable()) {
-        IMU.readGyroscope(x, y, z);
-
-        Serial.print(x);
-        Serial.print('\t');
-        Serial.print(y);
-        Serial.print('\t');
-        Serial.println(z);
-    }
+    // check the network connection once every 10 seconds:
+    delay(10000);
+    printCurrentNet();
 }
